972ea8b99df8e295287b3a5f235a96822f2528bb
777reet <harreetkaurh@gmail.com>
2025-05-23 17:29:47 +0530

    added a js page

---
+++ pages/script.js
@@ -0,0 +1,7 @@
+const Notes = document.querySelector(".notes");
+const button = document.querySelector(".btn");
+let note = document.querySelectorAll(".input");
+
+
+
+//code for interview : projects, engineering digest
